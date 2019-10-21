d2cd624269a1d3951a0db499e1808f50dce57f87
wongil12 <judgevi52@gmail.com>
2019-10-21 22:13:05 +0900

    Updates

---
+++ precache-manifest.78981d57cfe2eaa67c7dcde432f1f892.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391bde8f03ae73dbe0e2985b4f4a1744",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "b17dd27d9361b7354728",
+    "url": "/movie_app/static/css/main.d247dcdd.chunk.css"
+  },
+  {
+    "revision": "dfa343e37aa895ab1e37",
+    "url": "/movie_app/static/js/2.7aafe7cf.chunk.js"
+  },
+  {
+    "revision": "b17dd27d9361b7354728",
+    "url": "/movie_app/static/js/main.89e666e2.chunk.js"
+  },
+  {
+    "revision": "8971f8699b511f3e262b",
+    "url": "/movie_app/static/js/runtime-main.2890315e.js"
+  }
+]);
